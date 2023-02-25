d35cc218f399da77f528291d3d8768cacfc17780
Jona Joy <jonajoy142@gmail.com>
2023-02-25 09:11:30 +0530

    404 page addded

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     
-   <div>
+   <div className={styles.title}>
    
-    <h1>Homepage</h1>
-    <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    <h1 className={styles.text}>Homepage</h1>
+    <h2 className={styles.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electroni</h2>
       <br>
       </br>
